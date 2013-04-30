2e68bbe2ab627eb480303681306dcb44800bd5e6
AnthonyDry <anthonydry@live.se>
2013-04-30 18:25:55 +0200

    Added touch support for faders.
    Have some strange bug when i try to normalize the touch value from the
    faders it turns out negative.. :(

--- midi-korg-microKONTROL/js/visualizer.js
+++ midi-korg-microKONTROL/js/visualizer.js
@@ -18,217 +18,150 @@ function noteOff( note ) {
 			e.classList.remove("pressed");
 	
 }
-
+//TODO: this need to be refactored it's 2 messy.
 function controller( number, value ) {
-	var SLIDER_MIN = 125;
-	var SLIDER_STEPS = 120;
+
 	switch (number) {
-		//KNOBS ROTARY.
+		//This is the cases for the knobs/rotary.
 		case 16:
-		//Changes colour of the display.
-			var e = document.getElementById("led1");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text1");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led1", "led-text1", value);
 			break;
 		case 17:
-		//Changes colour of the display.
-			var e = document.getElementById("led2");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text2");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led2", "led-text2", value);
 			break;
 		case 18:
-		//Changes colour of the display.
-			var e = document.getElementById("led3");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text3");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led3", "led-text3", value);
 			break;
 		case 19:
-		//Changes colour of the display.
-			var e = document.getElementById("led4");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text4");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led4", "led-text4", value);
 			break;
 		case 48:
-		//Changes colour of the display.
-			var e = document.getElementById("led5");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text5");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led5", "led-text5", value);
 			break;
 		case 49:
-		//Changes colour of the display.
-			var e = document.getElementById("led6");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text6");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led6", "led-text6", value);
 			break;
 		case 50:
-		//Changes colour of the display.
-			var e = document.getElementById("led7");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text7");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led7", "led-text7", value);
 			break;
 		case 51:
-		//Changes colour of the display.
-			var e = document.getElementById("led8");
-			e.classList.add("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text8");
-			var val = precise_round(value,2);
-			e.innerText = val;
+			ChangeKnob("led8", "led-text8", value);
 			break;
-			
+			//This is the cases for the faders/sliders.
 		case 80:
-		//Changes colour of the display.
-			var e = document.getElementById("led1");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text1");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s80");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led1","led-text1","s80", value);
 			break;
-			
 		case 81:
-		//Changes colour of the display.
-			var e = document.getElementById("led2");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text2");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s81");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led2","led-text2","s81", value);
 			break;
 			
 		case 82:
-		//Changes colour of the display.
-			var e = document.getElementById("led3");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text3");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s82");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led3","led-text3","s82", value);
 			break;
 			
 		case 83:
-		//Changes colour of the display.
-			var e = document.getElementById("led4");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text4");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s83");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led4","led-text4","s83", value);
 			break;
 			
 		case 87:
-		//Changes colour of the display.
-			var e = document.getElementById("led5");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text5");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s87");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led5","led-text5","s87", value);
 			break;
 			
 		case 88:
-		//Changes colour of the display.
-			var e = document.getElementById("led6");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text6");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s88");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led6","led-text6","s88", value);
 			break;
 			
 		case 89:
-		//Changes colour of the display.
-			var e = document.getElementById("led7");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text7");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s89");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led7","led-text7","s89", value);
 			break;
 			
 		case 90:
-		//Changes colour of the display.
-			var e = document.getElementById("led8");
-			e.classList.remove("ledChange");
-			//Presents the value of the midi CC#
-			e = document.getElementById("led-text8");
-			var val = precise_round(value,2);
-			e.innerText = val;
-			//visualize slider movement.
-			var slider = document.getElementById("s90");
-			var movement = SLIDER_STEPS * val;
-			movement = SLIDER_MIN - movement;
-			movement = precise_round(movement,0);
-			slider.style.top = movement+"px";
+			ChangeFader("led8","led-text8","s90", value);
 			break;
 		
 		
 	}
 }
-
-function precise_round(num,decimals){
-return Math.round(num*Math.pow(10,decimals))/Math.pow(10,decimals);
+// HELP FUNCTIONS
+function ChangeKnob(el,text,value)
+{
+	//Changes colour of the display.
+	var e = document.getElementById(el);
+	e.classList.remove("ledChange");
+	//Presents the value of the midi CC#
+	e = document.getElementById(text);
+	var val = precise_round(value,2);
+	e.innerText = val;
 }
+function ChangeFader(el,text,obj,value)
+{
+	//CONSTANTS
+	var SLIDER_MIN = 125;
+	var SLIDER_STEPS = 120;
+	
+	//Changes colour of the display.
+	var e = document.getElementById(el);
+	e.classList.add("ledChange");
+	
+	//Presents the value of the midi CC#
+	e = document.getElementById(text);
+	var val = precise_round(value,2);
+	e.innerText = val;
+	
+	//visualize slider movement.
+	var slider = document.getElementById(obj);
+	var movement = SLIDER_STEPS * val;
+	movement = SLIDER_MIN - movement;
+	movement = precise_round(movement,0);
+	slider.style.top = movement+"px";
+}
+//This sets the touchbased interations led display.
+
+function precise_round(num,decimals)
+{
+	return Math.round(num*Math.pow(10,decimals))/Math.pow(10,decimals);
+}
+
+
+$(function() {
+	var dragableFaders = ["#s80","#s81","#s82","#s83","#s87","#s88","#s89","#s90"];
+	
+	for(var i = 0; i < dragableFaders.length; i++)
+	{
+	
+    	$(dragableFaders[i]).draggable({
+    		axis: "y", 
+    		containment: "parent",
+    		drag:function(event, ui){
+    			var led = "";
+    			var ledText = "";
+    			var currentPosition = ui.position.top - 125;
+    			var controlId = this.id.substring(1);
+    			//Oh god this i s worse then terrible! will fix soon
+				if(controlId == 80){led = "led1"; ledText = "led-text1";}
+				else if(controlId == 81){led = "led2"; ledText = "led-text2";}
+				else if(controlId == 82){led = "led3"; ledText = "led-text3";}
+				else if(controlId == 83){led = "led4"; ledText = "led-text4";}
+				else if(controlId == 87){led = "led5"; ledText = "led-text5";}
+				else if(controlId == 88){led = "led6"; ledText = "led-text6";}
+				else if(controlId == 89){led = "led7"; ledText = "led-text7";}
+				else if(controlId == 90){led = "led8"; ledText = "led-text8";}
+	    		
+	    		//Changes colour of the display.
+				var e = document.getElementById(led);
+				e.classList.add("ledChange");
+				
+				
+				e = document.getElementById(ledText);
+				//this is terrible but will fix it when i got time for it.
+				/*
+				 * For some stupid reason I get negatives when i do this normalization what is it that I'm missing??====
+				 */
+				var num = currentPosition / 120;
+				var val = Math.round(num*Math.pow(10,2))/Math.pow(10,2);
+				e.innerText = val;
+	    		
+    		},
+    	});
+   	}
+});
